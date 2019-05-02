f4c91c41d6f117efb1a10de7ed539f6dc72ca3b3
tarnag57 <toth.viktor7400@gmail.com>
2019-05-02 14:25:13 +0100

    A simple app

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,16 @@
 package sample;
 
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextArea;
+
 public class Controller {
+
+    @FXML
+    private TextArea bigTextArea;
+
+    @FXML
+    protected void ClickMeHandler(Event event) {
+        bigTextArea.setText("You have clicked me! Here's a cookie as a reward :)");
+    }
 }
