e631f682504f4636822c55e3bfc5eedcdfcaab76
tarnag57 <toth.viktor7400@gmail.com>
2019-05-07 17:34:34 +0100

    Creates skeleton

---
+++ src/sample/DataQuery.java
@@ -0,0 +1,9 @@
+package sample;
+
+public class DataQuery {
+
+    public static WeatherData queryData(String cityName) {
+        // TODO: Query each API and fill up WeatherData
+        return null;
+    }
+}
---
+++ src/sample/WeatherData.java
@@ -0,0 +1,7 @@
+package sample;
+
+public class WeatherData {
+
+    // Data fields
+    // TODO
+}
