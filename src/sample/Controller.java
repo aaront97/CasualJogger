5ab44d3b4c207f494a802a52267478177c76cf57
tarnag57 <toth.viktor7400@gmail.com>
2019-05-07 15:55:50 +0100

    Updates gitignore and plays around

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,7 +2,9 @@ package sample;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TextArea;
+import javafx.stage.Window;
 
 public class Controller {
 
@@ -11,6 +13,13 @@ public class Controller {
 
     @FXML
     protected void ClickMeHandler(Event event) {
-        bigTextArea.setText("You have clicked me! Here's a cookie as a reward :)");
+        bigTextArea.setText("You have clicked me! Here's a cookie as a reward :) Blah blah " +
+                ";sakfajkbrfk,wbfjwebfgajrs,hasmjrhgbUIRWGWlefh,kEWFH,WEFJ");
+//        Window owner = bigTextArea.getScene().getWindow();
+//        Alert alert = new Alert(Alert.AlertType.ERROR);
+//        alert.setTitle("No more cookies :(");
+//        alert.setContentText("Unfortunately, we have ran out of cookies. Apologies.");
+//        alert.initOwner(owner);
+//        alert.show();
     }
 }
