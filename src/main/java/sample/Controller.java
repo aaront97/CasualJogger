08bb3381e5574fd04f953fe2d4e8014781f14c94
tarnag57 <toth.viktor7400@gmail.com>
2019-05-14 14:06:07 +0100

    Continues design

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -2,17 +2,12 @@ package sample;
 
 import javafx.event.Event;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
 
 public class Controller {
 
     @FXML
-    private TextArea bigTextArea;
-
-    @FXML
     protected void ClickMeHandler(Event event) {
-        bigTextArea.setText("You have clicked me! Here's a cookie as a reward :) Blah blah " +
-                ";sakfajkbrfk,wbfjwebfgajrs,hasmjrhgbUIRWGWlefh,kEWFH,WEFJ");
+        System.out.println("Test");
 //        Window owner = bigTextArea.getScene().getWindow();
 //        Alert alert = new Alert(Alert.AlertType.ERROR);
 //        alert.setTitle("No more cookies :(");
