e686a14d95c0ca14c7c69b4e916025716b251ed7
aaront97 <33289351+aaront97@users.noreply.github.com>
2019-05-16 16:30:24 +0100

    added wind direction

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -7,6 +7,7 @@ import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import sample.api.DataQuery;
 import sample.api.WeatherData;
 
@@ -72,6 +73,9 @@ public class Controller {
     Label pollenCount;
 
     @FXML
+    ImageView windBearing;
+
+    @FXML
     protected void ClickMeHandler(Event event) {
         System.out.println(lowerToggle.isSelected());
     }
@@ -117,6 +121,14 @@ public class Controller {
         barChartPrecip.getData().add(barSeries);
         barChartPrecip.setBarGap(0);
         barChartPrecip.setCategoryGap(0);
+
+
+        windSpeed.setText(Math.round(weatherData.currentWindSpeed) + "mph");
+        windBearing.setRotate(windBearing.getRotate() + weatherData.currentWindBearing);
+        uvIndex.setText(weatherData.currentUV + "");
+        airQuality.setText(Math.round(weatherData.currentAQI) + "");
+        pollenCount.setText(weatherData.currentPollen);
+
     }
 
     @FXML
